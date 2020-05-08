fd9626bf3ecbf857bd76fb83eed5e0590e1d11af
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-08 22:42:58 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,10 +4,10 @@ def square_array(numbers)
 
   numbers.each do |number|
     number = number ** 2 
-    result.push(number)
+    numbers.push(number)
   end
   
-  return result
+  return numbers
   
 end
 
